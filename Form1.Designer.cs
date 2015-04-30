a8e441c809b202af013c9e9191aa16c45f14c27e
ricodejong02 <ricodejong02@gmail.com>
2015-04-30 12:55:26 +0200

    Initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Asker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.AskBox = new System.Windows.Forms.RichTextBox();
+            this.OutputBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(274, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ask";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AskBox
+            // 
+            this.AskBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.AskBox.Location = new System.Drawing.Point(12, 340);
+            this.AskBox.Name = "AskBox";
+            this.AskBox.Size = new System.Drawing.Size(256, 23);
+            this.AskBox.TabIndex = 1;
+            this.AskBox.Text = "";
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OutputBox.Location = new System.Drawing.Point(12, 3);
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.ReadOnly = true;
+            this.OutputBox.Size = new System.Drawing.Size(337, 331);
+            this.OutputBox.TabIndex = 2;
+            this.OutputBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 365);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.AskBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Asker";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox AskBox;
+        private System.Windows.Forms.RichTextBox OutputBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //Add something like this:
+            if (AskBox.Text == "How are you?")
+            {
+                AskBox.Clear();
+                OutputBox.AppendText("I am fine.\r\n");
+            }
+
+            //Or add little games like this:
+            if (AskBox.Text == "Rock!")
+            {
+                switch(random.Next(3))
+                {
+                    case 0:
+                        AskBox.Clear();
+                        OutputBox.AppendText("Rock! A draw.\r\n");
+                        break;
+                    case 1:
+                        AskBox.Clear();
+                        OutputBox.AppendText("Paper! I win.\r\n");
+                        break;
+                    case 2:
+                        AskBox.Clear();
+                        OutputBox.AppendText("Scissors! You win.\r\n");
+                        break;
+                }
+            }
+
+            if (AskBox.Text == "Paper!")
+            {
+                switch (random.Next(3))
+                {
+                    case 0:
+                        AskBox.Clear();
+                        OutputBox.AppendText("Rock! You win.\r\n");
+                        break;
+                    case 1:
+                        AskBox.Clear();
+                        OutputBox.AppendText("Paper! A draw.\r\n");
+                        break;
+                    case 2:
+                        AskBox.Clear();
+                        OutputBox.AppendText("Scissors! I win.\r\n");
+                        break;
+                }
+            }
+
+            if (AskBox.Text == "Scissors!")
+            {
+                switch (random.Next(3))
+                {
+                    case 0:
+                        AskBox.Clear();
+                        OutputBox.AppendText("Rock! I win.\r\n");
+                        break;
+                    case 1:
+                        AskBox.Clear();
+                        OutputBox.AppendText("Paper! You win.\r\n");
+                        break;
+                    case 2:
+                        AskBox.Clear();
+                        OutputBox.AppendText("Scissors! A draw.\r\n");
+                        break;
+                }
+            }
+        }
+    }
+}
